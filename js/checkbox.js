dd2401e57bb03d6ec4bb7e3e8dea055973dbd60c
MaksymChukhrai <106784292+MaksymChukhrai@users.noreply.github.com>
2024-03-03 21:25:31 +0200

    pc in progress

--- js/checkbox.js
+++ js/checkbox.js
@@ -1,6 +1,9 @@
 // Получаем все чекбоксы
 const checkboxInputs = document.querySelectorAll('.visually-hidden');
 
+// Определяем текущее окружение (локальное или не локальное)
+const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+
 // Проходимся по каждому чекбоксу и добавляем обработчик события
 checkboxInputs.forEach(checkboxInput => {
   // Получаем соответствующий иконке чекбокса элемент
@@ -10,11 +13,12 @@ checkboxInputs.forEach(checkboxInput => {
   checkboxInput.addEventListener('change', (event) => {
     if (event.target.checked) {
       // Меняем иконку на нажатое состояние
-      checkboxIcon.setAttribute('href', '/Conveyor-test-task/img/icons/sprite.svg#icon-checkbox-2');
+      const iconPath = isLocalhost ? '../../Conveyor-test-task/img/icons/sprite.svg#icon-checkbox-2' : '../img/icons/sprite.svg#icon-checkbox-2';
+      checkboxIcon.setAttribute('href', iconPath);
     } else {
       // Возвращаем исходную иконку
-      checkboxIcon.setAttribute('href', '/Conveyor-test-task/img/icons/sprite.svg#icon-checkbox');
+      const iconPath = isLocalhost ? '../../Conveyor-test-task/img/icons/sprite.svg#icon-checkbox' : '../img/icons/sprite.svg#icon-checkbox';
+      checkboxIcon.setAttribute('href', iconPath);
     }
   });
 });
-
