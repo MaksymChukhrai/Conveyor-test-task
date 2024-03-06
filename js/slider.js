5dec7a926246e1a65313776bc434d1b3492126bf
MaksymChukhrai <106784292+MaksymChukhrai@users.noreply.github.com>
2024-03-06 03:17:25 +0200

    backed up

--- js/slider.js
+++ js/slider.js
@@ -1,19 +1,20 @@
-document.addEventListener('DOMContentLoaded', function () {
-// Initialise the main slider
-  const swiperMain = new Swiper('.swiper', {
+document.addEventListener("DOMContentLoaded", function () {
+  // Initialise the main slider
+  const swiperMain = new Swiper(".swiper", {
     slidesPerView: 1,
     spaceBetween: 10,
     loop: true,
-  //   scrollbar: {
-  //     el: '.swiper-scrollbar',
-  //     hide: true, // Скрыть скроллбар
-  // },
-    // pagination: {
-    //   el: '.swiper-pagination',
-    // },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
     autoplay: {
       delay: 2000, // Delay between slides in milliseconds (in this case 5 seconds)
       disableOnInteraction: false, // Disable autoscrolling when user interacts with the slider
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
     },
     speed: 1000,
     breakpoints: {
@@ -22,12 +23,21 @@ document.addEventListener('DOMContentLoaded', function () {
         slidesPerView: 1,
         spaceBetween: 10,
         loop: true,
+        navigation: {
+          nextEl: "swiper-button-disabled",
+          prevEl: "swiper-button-disabled",
+        },
       },
       // when window width is >= 1200px
       1200: {
         slidesPerView: 1,
         spaceBetween: 10,
-      }
-    }
+        loop: true,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      },
+    },
   });
 });
