46726b3bcd75b87dbefdabd26d64be53dbdf9674
MaksymChukhrai <106784292+MaksymChukhrai@users.noreply.github.com>
2024-03-10 01:33:18 +0200

    new local

--- js/proba.js
+++ js/proba.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', function () {
   const checkboxInputs = document.querySelectorAll(".visually-hidden");
 
   // Указываем абсолютный путь к файлу спрайта
-  const iconPathChecked = "https://maksymchukhrai.github.io/Conveyor-test-task/img/icons/sprite.svg#icon-checkbox-2";
-  const iconPathUnchecked = "https://maksymchukhrai.github.io/Conveyor-test-task/img/icons/sprite.svg#icon-checkbox";
+  const iconPathChecked = "https://maksymchukhrai.github.io/Conveyor-test-task/blob/main/img/icons/Checkbox_cheked.png";
+  const iconPathUnchecked = "https://maksymchukhrai.github.io/Conveyor-test-taskblob/blob/main/img/icons/Checkbox.png";
 
   // Добавляем обработчик события для каждого чекбокса
   checkboxInputs.forEach(checkboxInput => {
