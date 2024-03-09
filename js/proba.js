ed60d8cc53e53b845bdfde8bcdc2e46d000f61dd
MaksymChukhrai <106784292+MaksymChukhrai@users.noreply.github.com>
2024-03-10 01:53:57 +0200

    cheked

--- js/proba.js
+++ js/proba.js
@@ -3,22 +3,22 @@ document.addEventListener('DOMContentLoaded', function () {
   const checkboxInputs = document.querySelectorAll(".visually-hidden");
 
   // Указываем абсолютный путь к файлу спрайта
-  const iconPathChecked = "https://maksymchukhrai.github.io/Conveyor-test-task/blob/main/img/icons/Checkbox_cheked.png";
-  const iconPathUnchecked = "https://maksymchukhrai.github.io/Conveyor-test-taskblob/blob/main/img/icons/Checkbox.png";
+  const iconPathChecked = "../img/icons/Checkbox_cheked.png";
+  const iconPathUnchecked = "../img/icons/Checkbox.png";
 
   // Добавляем обработчик события для каждого чекбокса
   checkboxInputs.forEach(checkboxInput => {
     // Получаем элемент иконки чекбокса
-    const checkboxIcon = checkboxInput.parentElement.querySelector(".checkbox-icon svg use");
+    const checkboxIcon = checkboxInput.parentElement.querySelector(".checkbox-icon img");
 
     // Добавляем обработчик события изменения состояния чекбокса
     checkboxInput.addEventListener("change", event => {
       if (event.target.checked) {
         // Устанавливаем путь к иконке чекбокса при выборе
-        checkboxIcon.setAttribute("href", iconPathChecked);
+        checkboxIcon.setAttribute("src", iconPathChecked);
       } else {
         // Устанавливаем путь к иконке чекбокса при снятии выбора
-        checkboxIcon.setAttribute("href", iconPathUnchecked);
+        checkboxIcon.setAttribute("src", iconPathUnchecked);
       }
     });
   });
